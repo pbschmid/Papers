1d05802185a8131ef565fbc04fd713cf77842047
pbschmid <philippeschmid@gmx.net>
2015-02-02 19:40:20 +0100

    Initial Commit

---
+++ Papers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Papers
+//
+//  Created by Philippe Schmid on 02.02.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Papers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Papers
+//
+//  Created by Philippe Schmid on 02.02.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
