1a39e6c15415d7326f9f9786e5fcc109c2ac5c63
pbschmid <philippeschmid@gmx.net>
2015-03-25 21:06:33 +0100

    Add a DetailViewController to the project

---
+++ Papers/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
