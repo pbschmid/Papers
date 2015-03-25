2c08af32673c026f12fa90cdb56936e9b9957550
pbschmid <philippeschmid@gmx.net>
2015-03-25 21:18:45 +0100

    Add ImageProcessor class for preprocessing the images (Tresholding)

---
+++ Papers/ImageProcessor.h
@@ -0,0 +1,13 @@
+//
+//  ImageProcessor.h
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageProcessor : NSObject
+
+@end
