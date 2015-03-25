ebc7c0e65910fa869343b00fbdd3a8a658c6b891
pbschmid <philippeschmid@gmx.net>
2015-03-25 21:21:26 +0100

    Add a Utility class to the project

---
+++ Papers/Utility.h
@@ -0,0 +1,13 @@
+//
+//  Utility.h
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utility : NSObject
+
+@end
---
+++ Papers/Utility.m
@@ -0,0 +1,13 @@
+//
+//  Utility.m
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import "Utility.h"
+
+@implementation Utility
+
+@end
