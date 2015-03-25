fd60f56778d86dd6744fb7d3486f824d892eb278
pbschmid <philippeschmid@gmx.net>
2015-03-25 21:12:39 +0100

    Add a DropboxClient class to the project

---
+++ Papers/DropboxClient.m
@@ -0,0 +1,13 @@
+//
+//  DropboxClient.m
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import "DropboxClient.h"
+
+@implementation DropboxClient
+
+@end
