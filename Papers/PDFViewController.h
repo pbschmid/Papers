d910017b484765b83e0b280688b1b36b9c774833
pbschmid <philippeschmid@gmx.net>
2015-03-25 21:07:17 +0100

    Add a PDFViewController to the project

---
+++ Papers/PDFViewController.h
@@ -0,0 +1,13 @@
+//
+//  PDFViewController.h
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDFViewController : UIViewController
+
+@end
---
+++ Papers/PDFViewController.m
@@ -0,0 +1,37 @@
+//
+//  PDFViewController.m
+//  Papers
+//
+//  Created by Philippe Schmid on 25.03.15.
+//  Copyright (c) 2015 Philippe Schmid. All rights reserved.
+//
+
+#import "PDFViewController.h"
+
+@interface PDFViewController ()
+
+@end
+
+@implementation PDFViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
